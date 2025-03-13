9318a34f4297032db820622057723ee786249482
rafael arcanjo <rafaelarcanjo129@hotmail.com>
2025-03-13 13:13:40 -0300

    ajustes nas pastas

--- src/A023-heranca/test.ts
+++
@@ -1,9 +0,0 @@
-import assert from 'node:assert'
-import { Aluno } from '.'
-
-
-const aluno = new Aluno('Rafa', 23, 'xxxxx')
-
-
-
-assert(aluno.getIdade() === 21, 'Deve retornar a idade da Class base')
